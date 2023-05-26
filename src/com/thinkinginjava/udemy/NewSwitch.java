9cd7db2a5e2f238bb861933b2a838e1a03aa6485
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-05-26 18:56:44 +0200

    add exercise to practice new SWITCH syntax

---
+++ src/com/thinkinginjava/udemy/NewSwitch.java
@@ -0,0 +1,26 @@
+package com.thinkinginjava.udemy;
+
+public class NewSwitch {
+
+    public static void main(String[] args) {
+
+        String month = "JANUARY";
+
+        System.out.println(month + " belongs to the " + returnQuarter(month) + " quarter of the year");
+    }
+
+    public static String returnQuarter(String month) {
+
+        return switch (month) {
+            case "JANUARY", "FEBRUARY", "MARCH" -> { yield "First"; }   // yield in code block
+            case "APRIL", "MAY", "JUNE" -> "Second";
+            case "JULY", "AUGUST", "SEPTEMBER" -> "Third";
+            case "OCTOBER", "NOVEMBER", "DECEMBER" -> "Fourth";
+            default -> {
+                String badResponse = month + " is not a valid month";
+                yield badResponse;      // yield has to be inside code block to work
+            }
+        };
+    }
+
+}
