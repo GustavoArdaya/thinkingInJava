12b3ec4d990721b225a50a0ff0795d70e896ae2b
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-05-29 01:01:01 +0200

    Prime Number Counter

---
+++ src/com/thinkinginjava/udemy/PrimeNumbers.java
@@ -0,0 +1,30 @@
+package com.thinkinginjava.udemy;
+
+public class PrimeNumbers {
+
+    public static void main(String[] args) {
+        int counter = 0;
+
+        for (int i = 1 ; i <= 1000; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+                counter++;
+            }
+            if (counter == 10) break;
+        }
+    }
+    public static boolean isPrime(int wholeNumber) {
+        if (wholeNumber <= 2) {
+            return (wholeNumber == 2);
+        }
+
+        for (int divisor = 2; divisor <= wholeNumber / 2; divisor++) {
+            if (wholeNumber % divisor == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
--- src/com/thinkinginjava/udemy/TraditionalSwitchChallenge.java
+++ src/com/thinkinginjava/udemy/TraditionalSwitchChallenge.java
@@ -38,9 +38,40 @@ public class TraditionalSwitchChallenge {
                 case 'y','Y': code += "Yoke "; break;
                 case 'z','Z': code += "Zebra "; break;
                 default: code += "";
-
+            
             }
         }
         return code;
+    }
+
+    public static int getDaysInMonth(int month, int year) {
+
+        if (year < 1 || year > 9999) return -1;
+
+        return switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12 -> 31;
+            case 2 -> (isLeapYear(year)) ? 29 : 28;
+            case 4, 6, 9, 11 -> 30;
+            default -> -1;
+        };
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year < 1 || year > 9999) {
+            return false;
+        } else {
+
+            boolean div_by_4 = (year % 4 == 0)? true : false;
+            boolean div_by_100 = (year % 100 == 0)? true: false;
+            boolean div_by_400 = (year % 400 == 0)? true: false;
+
+            if (div_by_4) {
+                if (div_by_100) {
+                    if (div_by_400) {
+                        return true;
+                    } else return false;
+                } else return true;
+            } else return false;
+        }
     }
 }
